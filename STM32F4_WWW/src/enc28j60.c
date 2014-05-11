963f03709e4be0cfa47c94351bcaf09dda18fdd1
Michal Ksiezopolski <xiezopolskim@gmail.com>
2014-05-11 21:11:20 +0200

    Added some reg defs.

---
+++ STM32F4_WWW/src/enc28j60.c
@@ -0,0 +1,20 @@
+/**
+ * @file: 	enc28j60.c
+ * @brief:	   
+ * @date: 	21 kwi 2014
+ * @author: Michal Ksiezopolski
+ * 
+ * @verbatim
+ * Copyright (c) 2014 Michal Ksiezopolski.
+ * All rights reserved. This program and the 
+ * accompanying materials are made available 
+ * under the terms of the GNU Public License 
+ * v3.0 which accompanies this distribution, 
+ * and is available at 
+ * http://www.gnu.org/licenses/gpl.html
+ * @endverbatim
+ */
+
+
+#include "enc28j60.h"
+#include "enc28j60_defs.h"
